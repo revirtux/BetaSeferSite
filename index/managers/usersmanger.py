3ad0c034d4fd4f728fa1f7bed874413c65fd7acd
Revirtux <ninjailbreak1@gmail.com>
2020-03-06 14:45:18 +0200

    Generic get & add users

--- index/managers/usersmanger.py
+++ index/managers/usersmanger.py
@@ -3,11 +3,15 @@ from functools import reduce
 
 from ..models.users import User, USER_STATES
 from ..models.solutions import Solution
+from ..models.houses import House
 from .playerutils import Subject, Player, get_rank
 
-def get_all_players() -> list:
+def get_all_players(state: str) -> list:
+    if state not in USER_STATES:
+        return []
+
     players = []
-    for user in User.objects.filter(state=USER_STATES['Player']):
+    for user in User.objects.filter(state=USER_STATES[state]):
         players.append(dox_user(user))
     return sorted(players, key=lambda p: p.total, reverse=True)
 
@@ -15,6 +19,7 @@ def dox_user(user: User):
     total, subjects = get_top_three(Solution.objects.filter(user=user))
     return Player(
         user.nick,
+        user.note,
         get_rank(total // 10),
         subjects[0],
         subjects[1],
@@ -31,3 +36,10 @@ def get_top_three(solutions: Solution) -> (int, list):
 
     return  reduce(lambda val, tup: tup[1] + val, categories.most_common(3), 0), \
             [Subject(name, score) for name, score in (categories.most_common(3) + [('Null', 0)] * 3)]
+
+def add_user(nick: str, state: str, houses: list = [], note: str = "", mail: str = "user@example.com", security: bool = False, ninja: bool = False) -> None:
+    user = User(nick=nick, state=state, note=note, mail=mail, security=security, ninja=ninja)
+    user.save()
+    for house in houses:
+        house_decleration = house[0].upper() + house[1:].lower()
+        user.houses.add(House.objects.get(name=house_decleration))
