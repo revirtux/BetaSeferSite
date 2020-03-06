194c23b5e4f87e30468c4bcfcb62b34634c4d2ef
Revirtux <ninjailbreak1@gmail.com>
2020-03-06 14:47:01 +0200

    Add scraping script

--- index/urls.py
+++ index/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('initdb', views.initdb, name='initdb'),
     path('cleardb', views.cleardb, name='cleardb'),
-    path('init_houses', views.init_houses, name='init_houses')
+    path('init_houses', views.init_houses, name='init_houses'),
+    path('scrap', views.scrap, name='scrap')
 ]
