14bf69f70b717a09bb013213b61c56f39613e1e5
REiNAgar <reinagar78@gmail.com>
2020-09-27 16:05:32 +0300

    this is my work from 27.09.2020

--- index/scripts/old_site_api.py
+++ index/scripts/old_site_api.py
@@ -42,8 +42,8 @@ def import_challenges():
         update_category(category['table_name'])
 
         for challenge in category['challenges']:
-            if '/' in challenge['points']:      # here I ignored codewars
-                challenge['points'] = 1     # its just for now to see if it works
+            if challenge['challenge_name'].lower() == "codewars":      # here I ignored codewars
+                continue
 
             update_challenge(challenge['challenge_name'],
                              category['table_name'],
@@ -51,39 +51,54 @@ def import_challenges():
                              int(challenge['points']),
                              challenge['deadline'])
 
-
 # update_solution(user: str, challenge: str, category: str, multipoint: int = 1):
 def import_solutions():
     all_solved_challenges = solved_challenges_table_organize()
 
     for category in all_solved_challenges:
         for challenge in category['challenges']:
+            if "codingbat" in challenge['challenge_name'].lower():
+                continue
             for solver in challenge['solvers']:     # added user it4n for it to work
+                if solver == "it4n":    # missing from the db
+                    continue
                 if challenge['challenge_name'] == "codewars":
                     update_codewars(solver, category['subject'])
-
                     continue
-                update_solution(solver, challenge['challenge_name'], category['subject'])
+                elif solver == "cugz (cugz_advanced_crackme_2)":    # cugz_crackme - ask regev
+                    break
+                update_solution(solver.replace(" ", ""), challenge['challenge_name'], category['subject'])
 
-
+# update_challenge(name: str, category: str, description: str = "", score: int = 1)
 def update_codewars(data, category):
     solver_data = data.replace(")", "(").split("(")
 
     name = solver_data[0]
     challenges = solver_data[1].replace(',', '').split()
 
-    challenges_data = [{"color": challenges[i], "points": int(challenges[i + 2])} for i in range(0, len(challenges), 3)]
+    challenges_data = [{"color": challenges[i], "points": int(challenges[i + 2])} 
+                        for i in range(0, len(challenges), 3)]
 
-    for codewars_challenge in challenges_data:
-        for color, points in codewars_challenge.items():
-            update_solution(name, "Codewars " + color, category['subject'], multipoint=points)
+    for codewars_challenge in range(len(challenges_data)):
+        color, points = challenges_data[codewars_challenge].values()
+        update_challenge("Codewars " + color.title() + " Challenges",
+                         category,
+                         score=codewars_challenge + 1)
+        update_solution(name.replace(" ", ""),
+                        "Codewars " + color.title() + " Challenges",
+                         category,
+                         multipoint=points)
 
 
-def update_multipoint(data, challenge_name, category):
+def update_multipoints(data, challenge_name, category):
     name, points = data.replace(')', '').split(" ")[0], data.replace(')', '').split(" ")[-1]
 
     update_solution(name, challenge_name, category, points)
 
+
+def cugz_crackme():
+    # wierd sctructre of solvers
+    pass
 
 def update_from_old_site():
     # ss = SiteScrapper()
