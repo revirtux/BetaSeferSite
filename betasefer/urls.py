953856dc99dabc0926c732085b48f8309a6aeb2b
AmitTPB <amittpb@gmail.com>
2021-01-23 16:49:21 +0200

    Last dumb commit, added challenges and games
    hello

--- betasefer/urls.py
+++ betasefer/urls.py
@@ -26,3 +26,4 @@ urlpatterns = [
 ] 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
