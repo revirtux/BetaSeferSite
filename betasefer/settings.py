f985653a2e103e81357a03ddbc805e9829a51b7e
Meitar <meitarr013@gmail.com>
2021-01-28 17:42:44 +0200

    debug only if secret key is dev

--- betasefer/settings.py
+++ betasefer/settings.py
@@ -24,7 +24,7 @@ _DEV_KEY = 'development-key'
 SECRET_KEY = os.getenv('SECRET_KEY', default=_DEV_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (SECRET_KEY == _DEV_KEY)  # debug mode only if the secret key is the dev's one
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
