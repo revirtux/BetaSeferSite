60a2ae0b6244c21ac946bce5b5efafcb62c5552f
Meitar <meitarr013@gmail.com>
2021-01-28 17:42:20 +0200

    get SECRET_KEY from env

--- betasefer/settings.py
+++ betasefer/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'censored'
+_DEV_KEY = 'development-key'
+SECRET_KEY = os.getenv('SECRET_KEY', default=_DEV_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
