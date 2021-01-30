68f25ffabdc558c86780fde64457ac7a09337eea
Revirtux <ninjailbreak1@gmail.com>
2021-01-30 15:04:33 +0200

    Revert "Removed silk"

--- betasefer/settings.py
+++ betasefer/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'index'
+    'index',
+    'silk'
 ]
 
 MIDDLEWARE = [
+    'silk.middleware.SilkyMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
